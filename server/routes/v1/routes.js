dd14d1324af31407d168e0fb08828af95e887414
Haneen Ftayeh <hftayeh@my.centennialcollge.ca>
2025-11-12 17:12:17 -0500

    orders endpoint

--- server/routes/v1/routes.js
+++ server/routes/v1/routes.js
@@ -1,10 +1,12 @@
 import express from "express"
 import listingRoutes from "./listings.js"
 import userRoutes from "./users.js"
+import orderRoutes from "./orders.js"
 
 const router = express.Router()
 
 router.use("/listings", listingRoutes)
 router.use("/users", userRoutes)
+router.use("/orders", orderRoutes)
 
 export default router
